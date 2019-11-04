7bea53d4bfda51b86cab952c595fbece77c260b8
Malco Freitas <malco@outlook.com>
2019-11-04 12:44:24 -0300

    Update Postagem.py

--- api/model/Postagem.py
+++ api/model/Postagem.py
@@ -5,9 +5,13 @@ from api.model.Usuario import Usuario
 class Postagem(models.Model):
     #relacionamento n-1 com Usuario
     autor = models.ForeignKey(Usuario, on_delete = models.CASCADE)
+    #Possui um texto
     texto = models.TextField()
+    #Possui uma data de criação
     dataCriada = models.DateTimeField(auto_now=True)
+    #Contém um número variável de comentários
     numeroComentarios = models.IntegerField(default=0)
+    #Contém um número variável de curtidas
     numeroCurtidas = models.IntegerField(default=0)
 
     def __str__(self):
