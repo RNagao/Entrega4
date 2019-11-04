1b904dfda4690441a2b4ae04b510454a192b7155
Malco Freitas <malco@outlook.com>
2019-11-04 12:25:56 -0300

    Update Curtida.py

--- api/model/Curtida.py
+++ api/model/Curtida.py
@@ -2,9 +2,11 @@ from django.db import models
 from api.model.Usuario import Usuario
 from api.model.Postagem import Postagem
 
-
+#Entidade Curtida
 class Curtida(models.Model):
+    #relacionamento n-1 com Usuario
     autor = models.ForeignKey(Usuario, on_delete = models.CASCADE)
+    #relacionamento n-1 com Postagem
     postagem = models.ForeignKey(Postagem, on_delete = models.CASCADE)
 
     def __str__(self):
