aeb55d0436d1625f4293b641cf439e2bd3df4d0d
Malco Freitas <malco@outlook.com>
2019-11-04 12:28:02 -0300

    Update Pontuacao.py

--- api/model/Pontuacao.py
+++ api/model/Pontuacao.py
@@ -1,9 +1,11 @@
 from django.db import models
 from api.model.Usuario import Usuario
 
-
+#Entidade Pontuacao - Contém a pontuação dos funcionários cadastrados
 class Pontuacao(models.Model):
+    #relacionamento 1-1 com Usuario
     funcionario = models.OneToOneField(Usuario, on_delete = models.CASCADE)
+    #recebe um valor inteiro de pontos com valor padrão inicial igual a 0
     pontos = models.IntegerField(default = 0)
 
     def __str__(self):
