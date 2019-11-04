4d0097fcc9b2a89fc14487360730ecc89e2dda20
Malco Freitas <malco@outlook.com>
2019-11-04 12:32:18 -0300

    Update Postagem.py

--- api/model/Postagem.py
+++ api/model/Postagem.py
@@ -1,7 +1,9 @@
 from django.db import models
 from api.model.Usuario import Usuario
 
+#Entidade Postagem - Texto com data de criação e autor que será divulgado através de notificação para outros usuários cadastrados
 class Postagem(models.Model):
+    #relacionamento n-1 com Usuario
     autor = models.ForeignKey(Usuario, on_delete = models.CASCADE)
     texto = models.TextField()
     dataCriada = models.DateTimeField(auto_now=True)
